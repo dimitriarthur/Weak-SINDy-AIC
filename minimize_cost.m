85fd35fc2bd4e5abc9d0d1c572754aad549a7f27
dimitriarthur <arthur.dimitri@ee.ufcg.edu.br>
2021-10-26 18:29:41 -0300

    general global minimum calculation

--- minimize_cost.m
+++ minimize_cost.m
@@ -3,14 +3,14 @@ function xi_sparse = minimize_cost(theta,sparsif_coeff,lambda,x_dot,min_type)
     if strcmp(min_type,'Ridge')
         % matrix formulation of the minimum of the cost function
         xi_sparse = pinv(theta'*theta - lambda*eye(min(size(theta))))*theta'*x_dot;
-        xi_sparse = sparsify_xi_ridge(xi_sparse,theta,sparsif_coeff,lambda,x_dot,2);
+        xi_sparse = sparsify_xi(xi_sparse,theta,sparsif_coeff,lambda,x_dot,2,'Ridge');
     elseif strcmp(min_type,'STLS')
         % least squares basic formulation
         xi_sparse = theta\x_dot;  % initial guess: Least-squares
         % lambda is our sparsification knob.
-        xi_sparse = sparsify_xi_stls(xi_sparse,theta,sparsif_coeff,x_dot,2);
+        xi_sparse = sparsify_xi(xi_sparse,theta,sparsif_coeff,lambda,x_dot,2,'STLS');
     elseif strcmp(min_type,'BPDN')
         xi_sparse=pinv(theta'*theta)*(theta'*x_dot-lambda*ones(min(size(theta)),1)/2);
-        xi_sparse = sparsify_xi_bpdn(xi_sparse,theta,sparsif_coeff,lambda,x_dot,2);
+        xi_sparse = sparsify_xi(xi_sparse,theta,sparsif_coeff,lambda,x_dot,2,'BPDN');
     end
 end
