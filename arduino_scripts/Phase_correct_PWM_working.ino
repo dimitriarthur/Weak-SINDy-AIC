e3a2e679a44ade78193e308e84370f834e484366
dimitriarthur <arthur.dimitri@ee.ufcg.edu.br>
2022-02-21 09:53:00 -0300

    LHC shuffle PWM

--- arduino_scripts/Phase_correct_PWM_working.ino
+++ arduino_scripts/Phase_correct_PWM_working.ino
@@ -1,22 +1,30 @@
+#include <time.h>
+
 const byte adcPin = 0;  // A0
 unsigned char val = PIN5;
 volatile int state = LOW;
 volatile int state_comp = LOW;
 volatile int cont = 0;
-volatile int results[22][6];
+volatile int results[22][7];
 volatile int results_2[22];
 volatile int raw_angle = 0;
+volatile int raw_PWM = 0;
 volatile int clk;
 volatile int csn;
 volatile int cont_bit_position=0;
 volatile int calibration_cont=0;
 volatile int calibration_value;
+volatile int cont_samples=0;
+volatile int cont_LHC_point=0;
 
+int array_indexes[3];
+int LHC_array[3][2] = {{70,100},{90,100},{110,100}};
 int bit_values[10] = {512, 256, 128, 64, 32, 16, 8, 1, 1, 1};
 int bit_cont[22] = {0,0,0,0,0,1,0,2,0,3,0,4,0,5,0,6,0,7,0,8,0,9};
 int angular_bits_array[22] = {0,0,0,512,0,256,0,128,0,64,0,32,0,16,0,8,0,4,0,2,0,1};
 int clk_signal_array[22] = {HIGH,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH};
 int csn_signal_array[22] = {HIGH,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW};
+
 const int MotorBreakPinA = 9;
 const int MotorSpeedPinA = 3;
 const int MotorPinA = 12;
@@ -24,8 +32,9 @@ const int ClkPin = 0B01000000;
 const int CsnPin = 0B00010000;
 
 void setup() {
-
-  setup_PWM(100);
+  
+  setup_PWM(LHC_array[array_indexes[cont_LHC_point]][0]);
+  shuffleIndexes(array_indexes,sizeof(array_indexes)/sizeof(array_indexes[0]));
   setup_motor();
   setup_sensor_ports();
   Serial.begin(9600);
@@ -53,8 +62,9 @@ void setup_PWM(int val_Reg) {
   // Timer/Counter 1 Control Register A
   TCCR1A = 0b10000001; //
   // Timer/Counter 1 Interrupt Mask Register
-  TIMSK1 |= 0b11; // enable compare match and TOV0 overflow
+  TIMSK1 |= 0b00000111; // enable compare match and TOV0 overflow
   OCR1A = val_Reg;
+  OCR1B = 50;
   // Timer/Counter 1 Control Register B
   TCCR1B = 0b00000011; // CSn = B11 -> 64 prescaler
   DDRC |= 0B00000001; //pin 37
@@ -70,44 +80,78 @@ ISR(TIMER1_COMPA_vect) {
   PORTA = state_comp; //channel 1 - pin 22
 }
 
+ISR(TIMER1_COMPB_vect){
+  //setup_PWM(100);
+}
+
 ISR(TIMER1_OVF_vect) {
+  
   state = !state;
   PORTC = state;
   results[cont][0] = (analogRead(A0));
+  raw_PWM += results[cont][0];
   results[cont][1] = (cont);
   generate_pulses();
+  
 }
 
 void generate_pulses(){
+  analogWrite(3,LHC_array[array_indexes[cont_LHC_point]][0]);
+  results[cont][6] = LHC_array[array_indexes[cont_LHC_point]][0];
   results[cont][2] = csn_signal_array[cont];
   digitalWrite(45,csn_signal_array[cont]);
   results[cont][3] = clk_signal_array[cont];
   digitalWrite(43,clk_signal_array[cont]);
   raw_angle += angular_bits_array[cont]*digitalRead(41);
   results[cont][4] = raw_angle;
+  results[cont][5] = cont_LHC_point;
   cont++;
   if (cont==22){
     calibration_cont++;
     if (calibration_cont==1)
       calibration_value = raw_angle;
     raw_angle=0;
+    if (cont_samples < LHC_array[cont_LHC_point][1])
+      cont_samples++;
+    else{
+      if(cont_LHC_point<sizeof(LHC_array)/sizeof(LHC_array[0])){
+        cont_LHC_point++;
+        cont_samples=0;
+      }
+    }
+  }
+}
+
+void shuffleIndexes(int array_indexes[], int limit){
+  int i;
+  srand(time(NULL));
+  
+  for (int i = 0; i < limit; i++) {     // fill array
+      array_indexes[i] = i;
+  }
+  
+  for (int i = 0; i < limit; i++) {    // shuffle array
+      int temp = array_indexes[i];
+      int randomIndex = rand() % limit;
+      array_indexes[i] = array_indexes[randomIndex];
+      array_indexes[randomIndex] = temp;
   }
 }
 
 
-
-
 void loop() {
-  while (cont < 22) {
+  if (cont_LHC_point<sizeof(LHC_array)/sizeof(LHC_array[0])){
+    while (cont < 22) {
+    }
+    
+    Serial.print(results[21][0]);
+    Serial.print("\t");
+    Serial.print(results[21][4]-calibration_value);
+    Serial.print("\t");
+    Serial.print(results[21][6]);
+    Serial.print("\n");
+    cont = 0;
   }
-  
-//  for (int i = 0; i < 22; i++) {
-//   
-//    Serial.println(results[i][0]);
-//  }
-  Serial.print(results[21][0]);
-  Serial.print("\t");
-  Serial.print(results[21][4]-calibration_value);
-  Serial.print("\n");
-  cont = 0;
+  else
+    analogWrite(3,0);
 }
